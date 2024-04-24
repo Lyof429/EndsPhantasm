e21d92409b0b65137afdcb15385f5c544408ba93
Lyof429 <florian.lyof@laposte.net>
2024-04-24 10:32:09 +0200

    building

--- src/main/java/net/lyof/phantasm/world/feature/ObsidianTowerStructure.java
+++ src/main/java/net/lyof/phantasm/world/feature/ObsidianTowerStructure.java
@@ -7,6 +7,8 @@ import net.lyof.phantasm.config.ConfigEntries;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.entity.MobSpawnerBlockEntity;
+import net.minecraft.entity.EntityType;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.StructureWorldAccess;
@@ -33,15 +35,18 @@ public class ObsidianTowerStructure extends Feature<CountConfig> {
         CountConfig config = context.getConfig();
         origin = world.getChunk(origin).getPos().getCenterAtY(origin.getY());
 
+        Phantasm.log("Hi " + origin);
+
         int maxy = config.getCount().get(random) + origin.getY();
+        maxy = maxy + 5 - maxy % 5;
 
         for (int sy = 0; sy <= maxy; sy++) {
             for (int sx = -7; sx < 8; sx++) {
                 for (int sz = -7; sz < 8; sz++) {
-                    if (sx*sx + sz*sz < 49 && (sx*sx + sz*sz >= 36 || sy == maxy)) {
+                    if (sx*sx + sz*sz < 49 && (sx*sx + sz*sz >= 36 || sy % 5 == 0)) {
                         Block block = Blocks.OBSIDIAN;
-                        double crying = sy / (maxy + 10.0);
-                        if (Math.random() + 0.1 < crying * crying)
+                        double crying = (sy - 60) / (maxy - 60.0);
+                        if (Math.random() + 0.1 < crying * crying && crying > 0)
                             block = Blocks.CRYING_OBSIDIAN;
                         else if (Math.random() < 0.2)
                             block = Math.random() < 0.5 ? ModBlocks.POLISHED_OBSIDIAN : ModBlocks.POLISHED_OBSIDIAN_BRICKS;
@@ -55,9 +60,28 @@ public class ObsidianTowerStructure extends Feature<CountConfig> {
                     }
                 }
             }
+            if (sy % 5 == 0 && sy != maxy) generateRoom(world, origin.withY(sy + 1));
         }
 
-        Phantasm.log("Hi " + context.getOrigin());
         return true;
+    }
+
+    public static void generateRoom(WorldAccess world, BlockPos center) {
+        Phantasm.log("generating room at " + center);
+        if (Math.random() < 0.4) {
+            BlockPos door = center.mutableCopy();
+            if (Math.random() <= 0.25) door = door.east(6);
+            else if (Math.random() <= 0.25) door = door.west(6);
+            else if (Math.random() <= 0.25) door = door.north(6);
+            else door = door.south(6);
+
+            world.setBlockState(door, Blocks.AIR.getDefaultState(), Block.NOTIFY_NEIGHBORS);
+            world.setBlockState(door.up(), Blocks.AIR.getDefaultState(), Block.NOTIFY_NEIGHBORS);
+        }
+
+        Phantasm.log("Putting spawner at " + center);
+        world.setBlockState(center, Blocks.SPAWNER.getDefaultState(), Block.NOTIFY_NEIGHBORS);
+        if (world.getBlockEntity(center) instanceof MobSpawnerBlockEntity spawner)
+            spawner.setEntityType(EntityType.ZOMBIE, world.getRandom());
     }
 }

618d9a45561050e1e49bdbfd909c2d6f9c260d2f
Lyof <florian.lyof@laposte.net>
2025-04-16 17:39:51 +0200

    optimized the end biome overrides

--- src/main/java/net/lyof/phantasm/mixin/MultiNoiseBiomeSourceMixin.java
+++ src/main/java/net/lyof/phantasm/mixin/MultiNoiseBiomeSourceMixin.java
@@ -1,5 +1,6 @@
 package net.lyof.phantasm.mixin;
 
+import net.lyof.phantasm.Phantasm;
 import net.lyof.phantasm.config.ConfigEntries;
 import net.lyof.phantasm.world.ModWorldGeneration;
 import net.minecraft.registry.entry.RegistryEntry;
@@ -11,20 +12,25 @@ import net.minecraft.world.biome.source.MultiNoiseBiomeSource;
 import net.minecraft.world.biome.source.util.MultiNoiseUtil;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(MultiNoiseBiomeSource.class)
 public abstract class MultiNoiseBiomeSourceMixin {
+    @Unique private Boolean isEnd = null;
+
     @Shadow protected abstract MultiNoiseUtil.Entries<RegistryEntry<Biome>> getBiomeEntries();
 
     @Inject(method = "getBiome", at = @At("HEAD"), cancellable = true)
     public void forceMainIsland(int x, int y, int z, MultiNoiseUtil.MultiNoiseSampler noise,
                                 CallbackInfoReturnable<RegistryEntry<Biome>> cir) {
 
-        if (!ConfigEntries.forceMainIsland || ModWorldGeneration.LOOKUP == null ||
-                this.getBiomeEntries().getEntries().stream().noneMatch(p -> p.getSecond().matchesKey(BiomeKeys.THE_END)))
+        if (this.isEnd == null)
+            this.isEnd = this.getBiomeEntries().getEntries().stream().anyMatch(p -> p.getSecond().matchesKey(BiomeKeys.THE_END));
+
+        if (!ConfigEntries.forceMainIsland || ModWorldGeneration.LOOKUP == null || !this.isEnd)
             return;
 
         int i = BiomeCoords.toBlock(x);

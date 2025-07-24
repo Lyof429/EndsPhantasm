bae2b58bff218643d1a038bc9097864ec6db0c10
Lyof <florian.lyof@laposte.net>
2025-07-24 16:53:52 +0200

    crystalline tools show their bonus in their tooltip

--- src/main/java/net/lyof/phantasm/mixin/client/ItemMixin.java
+++ src/main/java/net/lyof/phantasm/mixin/client/ItemMixin.java
@@ -1,6 +1,9 @@
-package net.lyof.phantasm.mixin;
+package net.lyof.phantasm.mixin.client;
 
+import net.lyof.phantasm.Phantasm;
+import net.lyof.phantasm.config.ConfigEntries;
 import net.lyof.phantasm.setup.ModTags;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.item.TooltipContext;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -19,7 +22,10 @@ import java.util.List;
 public class ItemMixin {
     @Inject(method = "appendTooltip", at = @At("HEAD"))
     public void showCrystalBonus(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context, CallbackInfo ci) {
-        if (stack.isIn(ModTags.Items.XP_BOOSTED))
+        if (stack.isIn(ModTags.Items.XP_BOOSTED) && MinecraftClient.getInstance().player != null) {
             tooltip.add(Text.translatable("tooltip.xp_boosted").formatted(Formatting.GREEN));
+            String bonus = Math.round((float) ConfigEntries.crystalXPBoost * MinecraftClient.getInstance().player.experienceLevel) + "%";
+            tooltip.add(Text.translatable("tooltip.xp_boosted.value", bonus).formatted(Formatting.GREEN));
+        }
     }
 }

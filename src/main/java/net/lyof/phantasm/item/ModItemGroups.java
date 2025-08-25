be0f2bf4cf44960854c0b68f08b9fc99033398e2
Lyof <florian.lyof@laposte.net>
2025-08-25 15:03:03 +0200

    trying to setup acidburnt ruins

--- src/main/java/net/lyof/phantasm/item/ModItemGroups.java
+++ src/main/java/net/lyof/phantasm/item/ModItemGroups.java
@@ -12,6 +12,7 @@ import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.text.Text;
+import net.minecraft.world.gen.structure.StructureType;
 
 public class ModItemGroups {
     public static final ItemGroup PHANTASM = register(Phantasm.MOD_ID,

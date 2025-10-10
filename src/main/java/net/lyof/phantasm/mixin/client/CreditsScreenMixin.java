6078c7493a055b0dbd94be070fa7c39cdb26b70d
Lyof <florian.lyof@laposte.net>
2025-10-10 15:17:58 +0200

    shortened the cutscene a bit

--- src/main/java/net/lyof/phantasm/mixin/client/CreditsScreenMixin.java
+++ src/main/java/net/lyof/phantasm/mixin/client/CreditsScreenMixin.java
@@ -40,7 +40,7 @@ public abstract class CreditsScreenMixin implements MixinAccess<Boolean> {
             this.centeredLines = new IntOpenHashSet();
             this.load("phantasm:texts/begin.txt", this::readPoem);
 
-            this.creditsHeight = this.credits.size() * 12 - 250;
+            this.creditsHeight = this.credits.size() * 12 - 200;
             ci.cancel();
         }
     }

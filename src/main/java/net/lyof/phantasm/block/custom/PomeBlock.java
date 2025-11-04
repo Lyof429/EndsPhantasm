a68e22c62eb6b904be58364328e41d4feed59702
Lyof <florian.lyof@laposte.net>
2025-11-04 21:56:51 +0100

    pomes now drop even when they fall

--- src/main/java/net/lyof/phantasm/block/custom/PomeBlock.java
+++ src/main/java/net/lyof/phantasm/block/custom/PomeBlock.java
@@ -1,12 +1,14 @@
 package net.lyof.phantasm.block.custom;
 
 import net.lyof.phantasm.effect.ModEffects;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.FallingBlock;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.FallingBlockEntity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.item.ItemStack;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.math.BlockPos;
@@ -23,14 +25,14 @@ public class PomeBlock extends FallingBlock {
     @Override
     public void randomDisplayTick(BlockState state, World world, BlockPos pos, Random random) {}
 
-    public int getColor(BlockState state, BlockView world, BlockPos pos) {
-        return state.getMapColor(world, pos).color;
-    }
-
     @Override
     protected void configureFallingBlockEntity(FallingBlockEntity entity) {
         super.configureFallingBlockEntity(entity);
         entity.setDestroyedOnLanding();
+    }
+
+    public int getColor(BlockState state, BlockView world, BlockPos pos) {
+        return state.getMapColor(world, pos).color;
     }
 
     @Override
@@ -43,7 +45,9 @@ public class PomeBlock extends FallingBlock {
             }
         }
 
-        world.addBlockBreakParticles(pos, this.getDefaultState());
+        if (world.isClient()) world.createExplosion(null, pos.getX() + 0.5, pos.getY(), pos.getZ() + 0.5,
+                2, World.ExplosionSourceType.NONE);
+        Block.dropStacks(this.getDefaultState(), world, pos, null, null, ItemStack.EMPTY);
         world.playSound(null, pos, SoundEvents.ENTITY_DRAGON_FIREBALL_EXPLODE, SoundCategory.BLOCKS);
         super.onDestroyedOnLanding(world, pos, fallingBlockEntity);
     }

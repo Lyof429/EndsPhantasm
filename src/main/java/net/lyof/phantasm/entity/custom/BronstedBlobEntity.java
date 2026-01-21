c23cb414779e4eaada7fbbb8df66b44917f44e0e
Lyof <florian.lyof@laposte.net>
2026-01-21 19:07:20 +0100

    bronsted blob water

--- src/main/java/net/lyof/phantasm/entity/custom/BronstedBlobEntity.java
+++ src/main/java/net/lyof/phantasm/entity/custom/BronstedBlobEntity.java
@@ -1,16 +1,24 @@
 package net.lyof.phantasm.entity.custom;
 
 import net.lyof.phantasm.block.ModBlocks;
+import net.lyof.phantasm.effect.ModEffects;
+import net.minecraft.entity.AreaEffectCloudEntity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.damage.DamageSources;
+import net.minecraft.entity.damage.DamageType;
+import net.minecraft.entity.damage.DamageTypes;
+import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.mob.HostileEntity;
 import net.minecraft.entity.mob.SlimeEntity;
 import net.minecraft.particle.BlockStateParticleEffect;
 import net.minecraft.particle.ParticleEffect;
 import net.minecraft.particle.ParticleTypes;
+import net.minecraft.registry.tag.DamageTypeTags;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldView;
 
@@ -49,12 +57,18 @@ public class BronstedBlobEntity extends SlimeEntity {
     public void setSize(int size, boolean heal) {
         super.setSize(size, heal);
         this.getAttributeInstance(EntityAttributes.GENERIC_MAX_HEALTH).setBaseValue((3+size)*size);
+        this.getAttributeInstance(EntityAttributes.GENERIC_ATTACK_DAMAGE).setBaseValue(2+size);
         if (heal) this.setHealth(this.getMaxHealth());
     }
 
     @Override
+    protected boolean canAttack() {
+        return this.canMoveVoluntarily();
+    }
+
+    @Override
     protected void updateStretch() {
-        this.targetStretch *= 0.8f;
+        this.targetStretch *= 0.95f;
     }
 
     @Override
@@ -65,6 +79,31 @@ public class BronstedBlobEntity extends SlimeEntity {
     @Override
     public boolean hurtByWater() {
         return !this.isSmall();
+    }
+
+    @Override
+    protected float modifyAppliedDamage(DamageSource source, float amount) {
+        if (source.isOf(DamageTypes.DROWN)) amount *= 5;
+        return super.modifyAppliedDamage(source, amount);
+    }
+
+    @Override
+    public boolean damage(DamageSource source, float amount) {
+        boolean b = super.damage(source, amount);
+        Random random = this.getRandom();
+        if (b && source.isOf(DamageTypes.DROWN) && random.nextFloat() < 0.1) {
+            AreaEffectCloudEntity cloud = EntityType.AREA_EFFECT_CLOUD.create(this.getWorld());
+            cloud.addEffect(new StatusEffectInstance(ModEffects.CORROSION, random.nextBetween(100, 400), random.nextInt(3)));
+            cloud.setOwner(this);
+            cloud.setPosition(this.getPos());
+            this.getWorld().spawnEntity(cloud);
+        }
+        return b;
+    }
+
+    @Override
+    public boolean canHaveStatusEffect(StatusEffectInstance effect) {
+        return super.canHaveStatusEffect(effect) && effect.getEffectType() != ModEffects.CORROSION;
     }
 
     @Override

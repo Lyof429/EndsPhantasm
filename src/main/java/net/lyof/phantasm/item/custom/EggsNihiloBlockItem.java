0ccb1753893ac43a3b8cce31733b8f916dd3b7c8
Lyof <florian.lyof@laposte.net>
2026-02-19 13:10:02 +0100

    fixed eggs nihilo being edible in item form

--- src/main/java/net/lyof/phantasm/item/custom/EggsNihiloBlockItem.java
+++ src/main/java/net/lyof/phantasm/item/custom/EggsNihiloBlockItem.java
@@ -3,10 +3,16 @@ package net.lyof.phantasm.item.custom;
 import net.lyof.phantasm.Phantasm;
 import net.minecraft.block.Block;
 import net.minecraft.client.item.TooltipContext;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.BlockItem;
+import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUsageContext;
 import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
 import net.minecraft.util.Formatting;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 import vectorwing.farmersdelight.common.utility.TextUtils;
@@ -16,6 +22,16 @@ import java.util.List;
 public class EggsNihiloBlockItem extends BlockItem {
     public EggsNihiloBlockItem(Block block, Settings settings) {
         super(block, settings);
+    }
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        return TypedActionResult.pass(user.getStackInHand(hand));
+    }
+
+    @Override
+    public ActionResult useOnBlock(ItemUsageContext context) {
+        return this.place(new ItemPlacementContext(context));
     }
 
     protected String descKey;
